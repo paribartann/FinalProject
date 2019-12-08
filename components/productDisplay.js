5c54f9952f093f6b156c833577a3fade397acb96
paribartann <paribartandhakal@gmail.com>
2019-12-08 14:28:14 -0600

    completed

--- components/productDisplay.js
+++ components/productDisplay.js
@@ -1,18 +1,15 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
-import { Table, Row, Rows } from 'react-native-table-component';
-import Searchbar from './searchBar';
-import ProductCategoryRow from "./ProductCategoryRow";
+import { StyleSheet, Text, View } from "react-native";
+import { Table, Row, Rows } from "react-native-table-component";
+import Searchbar from "./searchBar";
 import { connect } from "react-redux";
-import ProductRow from "./ProductRow";
 
 class ProductDisplay extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      tableHead: ['Name', 'Price']
-    }
+      tableHead: ["Name", "Price"]
+    };
   }
 
   render() {
@@ -21,8 +18,10 @@ class ProductDisplay extends Component {
     const inStockOnly = this.props.checkStock.inStockOnly;
 
     const rows = [];
+    let rowsCategory = [];
+    let rowsItem = [];
     let lastCategory = null;
-console.log("ADDD === ", this.props.add)
+    console.log("ADDD === ", this.props.add);
     const prod_array = this.props.add.sort((a, b) =>
       a.category > b.category ? 1 : -1
     );
@@ -34,25 +33,43 @@ console.log("ADDD === ", this.props.add)
         return;
       }
       if (product.category !== lastCategory) {
-        rows.push(
-          <ProductCategoryRow
-            category={product.category}
-            key={product.category}
-          />
+        rowsCategory.push(
+          <Text style={{ fontWeight: "bold", textAlign: "center" }}>
+            {product.category}
+          </Text>
         );
+        rows.push(rowsCategory);
+        rowsCategory = [];
       }
-      rows.push(<ProductRow product={product} key={Math.random()} />);
+
+      const name = product.stocked ? (
+        <Text style={{ textAlign: "center" }}>{product.name}</Text>
+      ) : (
+        <Text style={{ color: "red", textAlign: "center" }}>{product.name}</Text>
+      );
+
+      rowsItem.push(name);
+      rowsItem.push(product.price);
+      rows.push(rowsItem);
+      rowsItem = [];
       lastCategory = product.category;
-      console.log("ROWS === ", rows);
+      console.log("*****************************ROWS === ", rows);
     });
 
     return (
       <View>
         <Searchbar />
-        <Text style={{fontSize:24, fontWeight:"bold", textAlign:"center"}}>List of Items in our WareHouse</Text>
-        <Table borderStyle={{borderWidth: 2, borderColor: '#c8e1ff'}}>
-          <Row data={state.tableHead} style={styles.head} textStyle={styles.text}/>
-          {/* <Rows data={rows} textStyle={styles.text}/> */}
+        <Text style={{ fontSize: 24, fontWeight: "bold", textAlign: "center" }}>
+          List of Items in our WareHouse
+        </Text>
+
+        <Table borderStyle={{ borderWidth: 2, borderColor: "#c8e1ff" }}>
+          <Row
+            data={state.tableHead}
+            style={styles.head}
+            textStyle={styles.text}
+          />
+          <Rows data={rows} textStyle={styles.text} />
         </Table>
       </View>
     );
@@ -60,8 +77,8 @@ console.log("ADDD === ", this.props.add)
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, padding: 16, paddingTop: 30, backgroundColor: '#fff' },
-  head: { height: 40, backgroundColor: '#f1f8ff' },
+  container: { flex: 1, padding: 16, paddingTop: 30, backgroundColor: "#fff" },
+  head: { height: 40, backgroundColor: "#f1f8ff"},
   text: { margin: 6 }
 });
 
